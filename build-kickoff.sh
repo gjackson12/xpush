21dabf6675886a144167a3b8991fe96af942d2c9
%NAME% <%EMAIL%>
2018-10-20 17:07:48 -0400

    New build script

---
+++ build-kickoff.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Set variables for script
+S3_ARTIFACT_NAME="angular.zip"
+BUCKET_NAME="gjackson"
+
+echo "Zipping up project artifacts..."
+
+zip -r -X $S3_ARTIFACT_NAME .
+
+echo "Deleting project file from S3 Bucket..."
+
+s3cmd del s3://$BUCKET_NAME/$S3_ARTIFACT_NAME
+
+echo "Uploading project file to S3 Bucket..."
+
+s3cmd put $S3_ARTIFACT_NAME s3://$BUCKET_NAME/$S3_ARTIFACT_NAME
