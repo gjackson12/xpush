c8d4b387778d6726c518502d95484f8895503608
%NAME% <%EMAIL%>
2018-10-21 00:23:42 -0400

    Hello1

--- build-kickoff.sh
+++ build-kickoff.sh
@@ -5,14 +5,22 @@ DATETIMESTAMP=$(date +%F_%H-%M-%S)
 S3_ARTIFACT_NAME="angular$DATETIMESTAMP.zip"
 BUCKET_NAME="gjackson"
 
-echo "Zipping up project artifacts..."
+if [[ "$git_status" =~ On\ branch\ ([^[:space:]]+) ]]; then
+  branch=${BASH_REMATCH[1]}
 
-zip -r -X $S3_ARTIFACT_NAME .
+	if [branch == "master"]; then
+		echo "Zipping up project artifacts..."
 
-echo "Deleting project file from S3 Bucket..."
+		zip -r -X $S3_ARTIFACT_NAME .
 
-s3cmd del s3://$BUCKET_NAME/$S3_ARTIFACT_NAME
+		echo "Deleting project file from S3 Bucket..."
 
-echo "Uploading project file to S3 Bucket..."
+		s3cmd del s3://$BUCKET_NAME/$S3_ARTIFACT_NAME
 
-s3cmd put $S3_ARTIFACT_NAME s3://$BUCKET_NAME/$S3_ARTIFACT_NAME
+		echo "Uploading project file to S3 Bucket..."
+
+		s3cmd put $S3_ARTIFACT_NAME s3://$BUCKET_NAME/$S3_ARTIFACT_NAME
+	else
+		echo "No branch name found..."
+	fi
+fi
